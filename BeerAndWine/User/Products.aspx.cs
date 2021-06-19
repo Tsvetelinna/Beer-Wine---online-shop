0b07be39cf221b1465667b44aca0b4e41ee03453
Tsvetelinna <cvetilinna@gmail.com>
2021-06-20 02:35:14 +0300

    Fix search

--- BeerAndWine/User/Products.aspx.cs
+++ BeerAndWine/User/Products.aspx.cs
@@ -22,11 +22,17 @@ public partial class Product : System.Web.UI.Page
 
     private void BindProductRepeater()
     {
+        string search = "";
+        if (Request.QueryString["search"] != null)
+        {
+            search = Request.QueryString["search"];
+        }
+
         using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["BeerAndWineDB"].ConnectionString))
         {
             using (SqlCommand cmd = new SqlCommand("select A.*, B.*, B.Name as ImageName, C.Name as BrandName from Products A " +
-                "inner join Brands C on C.Id = A.BrandId left join ProductImages B on B.ProductId = A.Id  " +
-                "inner join Categories D on D.Id = A.CategoryId Order By A.Name Asc", con))
+               "inner join Brands C on C.Id = A.BrandId left join ProductImages B on B.ProductId = A.Id  " +
+               "inner join Categories D on D.Id = A.CategoryId Where A.Name like '%' + '" + search + "' + '%' Order By A.Name Asc", con))
             {
                 cmd.CommandType = CommandType.Text;
                 using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
--- BeerAndWine/User/User.master.cs
+++ BeerAndWine/User/User.master.cs
@@ -57,26 +57,6 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
     protected void btnsearch_Click(object sender, EventArgs e)
     {
-        using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["BeerAndWineDB"].ConnectionString))
-        {
-            if (txtSearch.Text == " ")
-            {
-                rptrProducts.DataSource = null;
-
-            }
-            using (SqlCommand cmd = new SqlCommand("select A.*, B.*, B.Name as ImageName, C.Name as BrandName from Products A " +
-                "inner join Brands C on C.Id = A.BrandId left join ProductImages B on B.ProductId = A.Id  " +
-                "inner join Categories D on D.Id = A.CategoryId  Where A.Name like '%' + '" + txtSearch.Text + "' + '%' Order By A.Name Asc", con))
-            {
-                cmd.CommandType = CommandType.Text;
-                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                {
-                    DataTable dt = new DataTable();
-                    sda.Fill(dt);
-                    rptrProducts.DataSource = dt;
-                    rptrProducts.DataBind();
-                }
-            }
-        }
+        Response.Redirect("~/User/Products.aspx?search="+ txtSearch.Text);
     }
 }
